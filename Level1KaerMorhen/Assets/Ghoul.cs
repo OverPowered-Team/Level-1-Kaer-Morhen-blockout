4b7ec885c27673cebb2c64cc8381bb5f0595aecd
thedoctormarc <mdpdoctor7@hotmail.com>
2020-03-01 23:14:02 +0100

    Ghouls. They emerge from nests upon player arriving

---
+++ Level1KaerMorhen/Assets/Ghoul.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum GhoulState { AWAIT, MOVE, STOP}
+
+public class Ghoul : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float travel = 3.5f;
+    public float detection = 8f;
+    public GameObject player; 
+    Vector3 startPos;
+    float speed = 5f; 
+    GhoulState state; 
+
+    void Start()
+    {
+        startPos = new Vector3();
+        startPos = gameObject.transform.position;
+        state = GhoulState.AWAIT;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (state == GhoulState.STOP)
+            return; 
+        else if (state == GhoulState.MOVE)
+        {
+            Vector3 targPos = transform.position + transform.forward * Time.deltaTime * speed;
+            if ((targPos - startPos).magnitude >= travel)
+                state = GhoulState.STOP;
+            
+            transform.position = targPos; 
+        }
+        else if (state == GhoulState.AWAIT)
+        {
+            if ((player) && ((player.transform.position - transform.position).magnitude <= detection))
+                state = GhoulState.MOVE; 
+        }
+
+    }
+}
